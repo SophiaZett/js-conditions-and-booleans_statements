6ea92a674961163fe2ba6ed48e5fcdd77bb6d5c2
Sophia S. <sophia0791@icloud.com>
2023-12-29 11:36:14 +0100

    add solutions

--- index.js
+++ index.js
@@ -5,22 +5,46 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD === receivedPassword) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
 // Part 2: Even / Odd
-const number = 6;
+const number = 7;
+
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
 
-// Part 4: Daytime
-const currentHour = 12;
+if (numberOfHotdogs <= 5) {
+  console.log(numberOfHotdogs * 2);
+} else if (numberOfHotdogs > 5 && numberOfHotdogs < 100) {
+  console.log(numberOfHotdogs * 1.5);
+} else if (numberOfHotdogs >= 100 && numberOfHotdogs < 1000000) {
+  console.log(numberOfHotdogs * 1);
+} else {
+  console.log(numberOfHotdogs * 0.1);
+}
 
-const statement = "";
+// Part 4: Daytime
+const currentHour = 19;
+
+let statement = "";
+
+statement = currentHour < 17 ? "Still need to learn..." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
-const name = "Archibald";
+const name = "Eileen";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (name === "Eileen" ? "Coach" : name) + "!";
 
 console.log(greeting);
